12938b94702b574839b5a49320cdec46a0a42c36
wingsit-desktop <wing1127aishi@gmail.com>
2009-07-18 14:20:27 -0400

    Construct cusum

--- import.py
+++ import.py
@@ -6,7 +6,7 @@ import scikits.timeseries.lib.reportlib as rl
 import scipy.stats.mstats as stats
 
 
-data = np.matrix(list(csv.reader(open("pmstar.csv", "r"))))
+data = np.matrix(list(csv.reader(open("spmstar.csv", "r"))))
 date = data[0,1:]
 desc = data[1:,0]
 data = np.array(data[1:,1:])
@@ -21,12 +21,36 @@ serieses = [i[i>-999] for i in [ma.masked_values(series, -999) for series in ser
 print len(serieses)
 
 s = serieses[1]
-for series in serieses:
-    try:
-        print rl.Report(series)()
-    except:
-        pass
+# for series in serieses:
+#     try:
+#         print rl.Report(series)()
+#     except:
+#         pass
+
+
 
 #print stats.gmean(s)
+#print s
+#print ma.mean(s[:36])
+
+def cumwindow(a, start = 0):
+    for  i in xrange(len(a[start:])):
+        yield a[0:i+start]
+
 print s
-print ma.mean(s)
+cusum = []
+
+for n, i in enumerate(cumwindow(s, 35)):
+    er = ma.mean(i)
+    std = ma.std(i, ddof = 1)
+    sar = (12.0 * s[n+35] - er) / (std * (12.0 ** 0.5))
+    cusum.append(sar)
+    print  s[n+35],er, std, sar, sum(cusum)
+
+
+# def rmean(timeseries, training_period = 36):
+#     print ma.mean(s[:training_period])
+#     for i in enumerate(s[36:]):
+#         print i
+    
+# print rmean(s)
